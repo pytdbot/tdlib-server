590470b01def6cbb32ee56a78d63141616d3c0d5
AYMENJD <53928879+AYMENJD@users.noreply.github.com>
2025-10-20 22:39:14 +0300

    Update version to 0.1.17.

--- internal/server/version.go
+++ internal/server/version.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pytdbot/tdlib-server/internal/utils"
 )
 
-const Version = "0.1.16"
+const Version = "0.1.17"
 const AppName = "TDLib Server"
 const MINIMUM_TDLIB_VERSION = "1.8.6"
 
